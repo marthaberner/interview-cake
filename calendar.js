c55c7a0261d027d15a165159215a2c04517f72a0
marthaberner <marthaberner.mb@gmail.com>
2016-08-26 11:43:28 -0600

    refactor #checkIfShouldBeCondensed to return condensed object or false

--- calendar.js
+++ calendar.js
@@ -1,4 +1,3 @@
-
 function sortMeetingsByStartTime(meetings) {
   meetings = meetings.sort(function (a, b) {
     if (a.startTime > b.startTime) {
@@ -14,9 +13,9 @@ function sortMeetingsByStartTime(meetings) {
     return meetings
 }
 
-function checkIfShouldBeCondensed(meetings) {
-  return meetings[0].endTime > meetings[1].startTime ?
-    [{startTime: meetings[0].startTime, endTime: meetings[1].endTime }] : meetings;
+function checkIfShouldBeCondensed(meeting1, meeting2) {
+  return meeting1.endTime > meeting2.startTime ?
+    {startTime: meeting1.startTime, endTime: meeting2.endTime } : false;
 }
 
 module.exports = {
--- test/calendar.test.js
+++ test/calendar.test.js
@@ -4,21 +4,16 @@ var funcs = require('../calendar');
 describe('Calendar', function () {
   describe('#checkIfShouldBeCondensed', function () {
     it('should not be condensed', function () {
-      var input = [
-        {startTime: 0, endTime: 1},
-        {startTime: 3, endTime: 5},
-      ]
-      assert.deepEqual(funcs.checkIfShouldBeCondensed(input), input);
+      var input1 = {startTime: 0, endTime: 1}
+      var input2 = {startTime: 3, endTime: 5}
+
+      assert.deepEqual(funcs.checkIfShouldBeCondensed(input1, input2), false);
     })
     it('should be condensed', function () {
-      var input = [
-        {startTime: 3, endTime: 5},
-        {startTime: 4, endTime: 8}
-      ]
-      var expected = [
-        {startTime: 3, endTime: 8}
-      ]
-      assert.deepEqual(funcs.checkIfShouldBeCondensed(input), expected)
+      var input1 = {startTime: 3, endTime: 5}
+      var input2 = {startTime: 4, endTime: 8}
+      var expected = {startTime: 3, endTime: 8}
+      assert.deepEqual(funcs.checkIfShouldBeCondensed(input1, input2), expected)
     });
   })
   describe('Sort Meetings', function () {
